ff5088167e23223585f8a76d1d707efaeefaed59
hanon11 <lucia.atienza2@gmail.com>
2021-03-27 19:08:17 +0100

    practica 2

--- practica2/clientes/ejercicio2_tcp_cli.py
+++ practica2/clientes/ejercicio2_tcp_cli.py
@@ -1,46 +1,76 @@
 import socket
 import os
+import json 
 HOST = 'localhost'
 PORT = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 print("*****Hola, CLIENTE*****")
-
-listaFicheros = os.listdir('.')
-hayPDF = 0
-for fich in listaFicheros:
-	if '.pdf' in fich:
-		hayPDF += 1
-if hayPDF == 0:
-	print("No existen ficheros con la extension .pdf para enviar, cerramos conexion.")
-	s.send("NO".encode("utf-8"))
-	s.close()
-
-else:
-	s.send("SI".encode("utf-8"))
-	print("Los ficheros disponibles para enviar son: ")
+accion = input("Que desea realizar? \n	1. Enviar fichero.\n	2. Descargar fichero del servidor.\n")
+s.send(accion.encode("utf-8"))
+if accion == '1': # enviar fichero
+	listaFicheros = os.listdir('.')
+	hayPDF = 0
 	for fich in listaFicheros:
+		if '.pdf' in fich: # compruebo si hay ficheros con extension .pdf en el directorio del cliente
+			hayPDF += 1
+
+	if hayPDF == 0:
+		print("No existen ficheros con la extension .pdf para enviar, cerramos conexion.")
+		s.send("NO".encode("utf-8"))
+		s.close()
+
+	else: # si exiten .pdfs
+		s.send("SI".encode("utf-8")) # aviso al servidor de que existen ficheros para mandar
+		print("Los ficheros disponibles para enviar son: ")
+		for fich in listaFicheros:
+			if '.pdf' in fich:
+				print("	○ " + fich)
+
+		FILE = input("Introduce el nombre del fichero: ")
+		if FILE not in listaFicheros:
+			print("Fichero erroneo. Cerramos conexion.")
+			s.close()
+		s.send(FILE.encode("utf-8"))
+		
+		confirmacion = s.recv(1024)		# Espera a la confirmación
+		print("Recibo el siguiente mensaje del servidor: " + confirmacion.decode("utf-8"))
+
+		f = open(FILE, "rb")
+		chunk = f.read(1024)
+		while chunk:
+			s.send(chunk)
+			chunk = f.read(1024)
+		f.close()
+
+		recepcion = s.recv(1024)
+		print("Recibo el siguiente mensaje del servidor: " + recepcion.decode("utf-8"))
+
+		os.remove(FILE)
+
+		s.close()
+
+if accion == '2':
+	ficherosServidor = s.recv(1024)
+	ficherosServidor = json.loads(ficherosServidor) # recibimos lista de ficheros del servidor
+	print("Los ficheros disponibles para recibir son: ")
+	for fich in ficherosServidor:
 		if '.pdf' in fich:
 			print("	○ " + fich)
-
-	FILE = input("Introduce el nombre del fichero: ")
-	s.send(FILE.encode("utf-8"))
-	
-	confirmacion = s.recv(1024)		# Espera a la confirmación
-	print(confirmacion.decode("utf-8"))
-
-	f = open(FILE, "rb")
-	chunk = f.read(1024)
-	while chunk:
-		s.send(chunk)
-		chunk = f.read(1024)
-	f.close()
-	os.remove(FILE)
-
-	msg = s.recv(1024)
-	msg = msg.decode("utf-8")
-	print(msg)
-	s.send("Po yo tambien".encode("utf-8"))
-
+	FILE_S = input("Introduce el nombre del fichero: ")
+	if FILE_S not in ficherosServidor:
+			print("Fichero erroneo. Cerramos conexion.")
+			s.close()
+	else:
+		s.send(FILE_S.encode("utf-8")) 
+		f = open(FILE_S, "wb")
+		while True:
+			chunk = s.recv(1024)
+			f.write(chunk)
+			if len(chunk) < 1024:
+				print("Fichero recibido.")
+				break
+		f.close()
+	print("Cerramos conexion")
 	s.close()
--- practica2/servidores/ejercicio2_tcp_ser.py
+++ practica2/servidores/ejercicio2_tcp_ser.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import json 
 HOST = 'localhost'
 PORT = 1024
 socketServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,27 +11,46 @@ print("*****SERVIDOR*****")
 print("Nos quedamos a la espera...")
 s_cliente, addr = socketServidor.accept()
 
-hayFichero = s_cliente.recv(1024)
-if hayFichero.decode("utf-8") == "SI":
-    # recibimos mensaje del fichero
-    FILE = s_cliente.recv(1024)
-    FILE = FILE.decode("utf-8")
-    print("Recibo:[" + FILE + "] del cliente con la direccion " + str(addr))
+eleccion = s_cliente.recv(1024)
+if eleccion.decode("utf-8") == '1': # el cliente envia
+    print("El cliente ha elegido enviar un fichero...")
+    hayFichero = s_cliente.recv(1024)
+    if hayFichero.decode("utf-8") == "SI":
+        # recibimos mensaje del fichero
+        FILE = s_cliente.recv(1024)
+        FILE = FILE.decode("utf-8")
+        print("Recibo:[" + FILE + "] del cliente con la direccion " + str(addr))
 
-    f = open(FILE, "wb")
-    s_cliente.send("Estoy listo".encode("utf-8"))
-    while True:
-        chunk = s_cliente.recv(1024)
-        f.write(chunk)
-        if len(chunk) < 1024:
-            print("Done Receiving.")
-            break
+        f = open(FILE, "wb")
+        s_cliente.send("Preparado para recibir fichero".encode("utf-8"))
+        while True:
+            chunk = s_cliente.recv(1024)
+            f.write(chunk)
+            if len(chunk) < 1024:
+                print("Fichero recibido.")
+                break
+        f.close()
+        s_cliente.send("Fichero recibido".encode("utf-8"))
+        s_cliente.close()
+    else:
+        print("El cliente no tiene nada para enviar :(. Cerramos conexion")
+        s_cliente.close()
+
+if eleccion.decode("utf-8") == '2': # el cliente recibe fichero
+    print("El cliente ha elegido recibir un fichero...")
+    listaFicheros = os.listdir('.')
+    send_txt = json.dumps(listaFicheros)
+    s_cliente.send(send_txt.encode("utf-8"))
+    fichero = s_cliente.recv(1024)
+    fichero = fichero.decode("utf-8")
+
+    f = open(fichero, "rb")
+    chunk = f.read(1024)
+    while chunk:
+        s_cliente.send(chunk)
+        chunk = f.read(1024)
     f.close()
-    s_cliente.send("Yata".encode("utf-8"))
-    mensaje = s_cliente.recv(1024)
-    print("Recibo:["+mensaje.decode("utf-8")+"] del cliente")
-    s_cliente.close()
-else:
-    print("El cliente no tiene nada para enviar :(. Cerramos conexion")
+    print("Fichero enviado al cliente")
+
     s_cliente.close()
 socketServidor.close()
