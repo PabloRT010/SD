781569c64c5ec52fb66c491adb509f52a82feef9
hanon11 <lucia.atienza2@gmail.com>
2021-04-18 11:33:12 +0200

    practica 2

--- practica2/clientes/clienteUDP.py
+++ practica2/clientes/clienteUDP.py
@@ -1,6 +1,6 @@
 import socket
 HOST = 'localhost'
 PORT = 1025
-s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s_udp.sendto("Soy el cliente".encode("utf-8"),(HOST, PORT))
-s_udp.close()
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cliente.sendto("Soy el cliente".encode("utf-8"),(HOST, PORT))  #send
+cliente.close()  #cerramos socket
--- practica2/servidores/servidorUDP.py
+++ practica2/servidores/servidorUDP.py
@@ -1,11 +1,11 @@
 import socket
 HOST = 'localhost'
 PORT = 1025
-s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s_udp.bind((HOST, PORT))
-print("Me quedo a la espera")
-mensaje,addr = s_udp.recvfrom(1024)
-print("Recibido el mensaje --->" + str(mensaje.decode("utf-8")))
-print("IP cliente: " + str(addr[0]))
-print("Puerto cliente: " + str(addr[1]))
-s_udp.close()
+servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #crear socket
+servidor.bind((HOST, PORT))  
+
+print("Servidor a la espera...")
+mensaje,addr = servidor.recvfrom(1024)  #receive
+print("Recibido el mensaje <<" + str(mensaje.decode("utf-8")) + ">> del cliente con la direccion: (" + str(addr[0]) + ", " + str(addr[1]) + ")")
+
+servidor.close()  #cerramos socket
