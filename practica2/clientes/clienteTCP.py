9b960956b1a1bde83650e47f6ee33057bf83f295
hanon11 <lucia.atienza2@gmail.com>
2021-04-18 11:22:55 +0200

    practica 2

--- practica2/clientes/clienteTCP.py
+++ practica2/clientes/clienteTCP.py
@@ -1,9 +1,11 @@
 import socket
 HOST = 'localhost'
 PORT = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send("Hola, servidor".encode("utf-8"))
-mensaje = s.recv(1024)
-print("Recibido:["+mensaje.decode("utf-8")+"] del servidor ")
-s.close() 
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
+
+cliente.send("Hola, soy el cliente".encode("utf-8"))
+mensaje = cliente.recv(1024)
+
+print("Recibido: <<"+mensaje.decode("utf-8")+">> del servidor ")
+cliente.close() 
--- practica2/servidores/servidorTCP.py
+++ practica2/servidores/servidorTCP.py
@@ -1,13 +1,15 @@
 import socket
 HOST = 'localhost'
 PORT = 1024
-socketServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socketServidor.bind((HOST, PORT))
-socketServidor.listen(1)
-print("Nos quedamos a la espera...")
-s_cliente, addr = socketServidor.accept()
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+servidor.listen(1)
+
+print("Servidor a la espera...")
+s_cliente, addr = servidor.accept()
 mensaje = s_cliente.recv(1024)
-print("Recibo:["+mensaje.decode("utf-8")+"] del cliente con la direccion " + str(addr))
+
+print("Recibo: <<"+mensaje.decode("utf-8")+">> del cliente con la direccion " + str(addr))
 s_cliente.send("Hola, cliente, soy el servidor".encode("utf-8"))
 s_cliente.close()
-socketServidor.close() 
+servidor.close() 
