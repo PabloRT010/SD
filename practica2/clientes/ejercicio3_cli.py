1766dd274187520f1c0e24f8f15548605423ccd3
hanon11 <lucia.atienza2@gmail.com>
2021-04-18 14:16:32 +0200

    practica 2

--- practica2/clientes/ejercicio3_cli.py
+++ practica2/clientes/ejercicio3_cli.py
@@ -5,17 +5,18 @@ PORT = 1025
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cliente.sendto("Soy el cliente".encode("utf-8"),(HOST, PORT))
 while True:
-    opciones = ["ls", "rm", "write", "cd", "mv", "exit"]
-    while True:
+    opciones = ["ls", "rm", "write", "cd", "mv", "exit"]  # opciones disponibles
+    while True:  # repetir hasta que exista la opcion introducida
         print("\n\nBienvenido, cliente. Sus opciones son: ")
         print(', '.join(opciones))
         eleccion = input()
-        if eleccion in opciones:
+        if eleccion in opciones:  # si el cliente introduce una opcion valida
             break
-        else:
+        else:  # si no existe
             print("El comando introducido no existe")
-    cliente.sendto(eleccion.encode("utf-8"),(HOST, PORT))
-    if eleccion == 'ls':
+    cliente.sendto(eleccion.encode("utf-8"),(HOST, PORT))  # enviamos al servidor la opcion
+
+    if eleccion == 'ls':  # el cliente elige listar el directorio del servidor
         lista, addr = cliente.recvfrom(1024)
         lista = pickle.loads(lista)
         print("Los ficheros y directorios que se encuentran en el directorio actual del servidor son: ")
@@ -25,14 +26,14 @@ while True:
             for fichero in lista:
                 print(fichero)
     
-    if eleccion == 'rm':
+    if eleccion == 'rm':  # el cliente selecciona borrar un fichero
         borrar = input("Introduce el nombre del fichero que desea borrar: ")
         cliente.sendto(borrar.encode("utf-8"),(HOST, PORT))
         mensaje, addr = cliente.recvfrom(1024)
         mensaje = mensaje.decode("utf-8")
         print("Recibo el siguiente mensaje del servidor: " + mensaje)
 
-    if eleccion == 'write':
+    if eleccion == 'write':  # el cliente selecciona crear un fichero
         nombre_fichero = input("Introduce el nombre del fichero que desea crear en el directorio del servidor: ")
         mensaje = input("Introduce el texto del fichero: ")
         cliente.sendto(nombre_fichero.encode("utf-8"),(HOST, PORT))
@@ -40,13 +41,13 @@ while True:
         mensaje, addr = cliente.recvfrom(1024)
         print("Recibo el siguiente mensaje del servidor: " + mensaje.decode("utf-8"))
 
-    if eleccion == 'cd':
+    if eleccion == 'cd':  # el cliente selecciona moverse en los directorios del servidor
         directorio = input("Introduce el nombre del directorio al que desea cambiarse del servidor: ")
         cliente.sendto(directorio.encode("utf-8"),(HOST, PORT))
         mensaje, addr = cliente.recvfrom(1024)
         print("Recibo el siguiente mensaje del servidor: " + mensaje.decode("utf-8"))
     
-    if eleccion == 'mv':
+    if eleccion == 'mv':  # el cliente selecciona mover un fichero
         origen = input("Introduce el origen: ")
         destino = input("Introduce el destino: ")
         cliente.sendto(origen.encode("utf-8"),(HOST, PORT))
--- practica2/servidores/ejercicio3_ser.py
+++ practica2/servidores/ejercicio3_ser.py
@@ -14,47 +14,47 @@ while True:
     listaFichero = os.listdir('.')
     opcion, addrCliente = s_udp.recvfrom(1024)  # eleccion del cliente
 
-    if opcion.decode("utf-8") == 'ls': 
+    if opcion.decode("utf-8") == 'ls':   # el cliente elige listar el directorio del servidor
         print("El cliente ha selecionado ls")
-        lista = pickle.dumps(listaFichero)
+        lista = pickle.dumps(listaFichero)  # envio de la lista de ficheros
         s_udp.sendto(lista, addrCliente)
 
-    if opcion.decode("utf-8") == 'rm': 
+    if opcion.decode("utf-8") == 'rm':  # el cliente elige borrar un fichero del directorio del servidor
         print("El cliente ha selecionado rm")
         fichero, addrCliente = s_udp.recvfrom(1024)  # recibe el nombre del fichero del cliente
         try:
-            os.remove(fichero)
+            os.remove(fichero)  # intentamos borrar fichero
             s_udp.sendto("El fichero ha sido borrado con exito".encode("utf-8"), addrCliente)
-        except FileNotFoundError:
+        except FileNotFoundError:  # si el fichero no existe
             s_udp.sendto("El fichero que desea borrar no existe.".encode("utf-8"), addrCliente)
-        except:
+        except:  # cualquier otro fallo
             s_udp.sendto("Algo salio mal.".encode("utf-8"), addrCliente)
 
-    if opcion.decode("utf-8") == 'write':
+    if opcion.decode("utf-8") == 'write':   # el cliente elige crear un fichero 
         print("El cliente ha selecionado write")
         fichero, addrCliente = s_udp.recvfrom(1024)
         mensaje, addrCliente = s_udp.recvfrom(1024)
         f = open(fichero.decode("utf-8"), "w+")
         try:
-            f.write(mensaje.decode("utf-8"))
+            f.write(mensaje.decode("utf-8"))  # intentamos escribir en el fichero
             s_udp.sendto("El fichero ha sido creado con exito".encode("utf-8"), addrCliente)
         except:
             s_udp.sendto("Algo salio mal".encode("utf-8"), addrCliente)
         finally:
             f.close()
     
-    if opcion.decode("utf-8") == 'cd':
+    if opcion.decode("utf-8") == 'cd':  # el cliente elige cambiar el directorio del servidor
         print("El cliente ha selecionado cd")
         directorio, addrCliente = s_udp.recvfrom(1024)
         try:
-            os.chdir(directorio.decode("utf-8"))
+            os.chdir(directorio.decode("utf-8"))  # intentamos cambiar de directorio
             s_udp.sendto("El cambio de directorio ha sido realizado con exito".encode("utf-8"), addrCliente)
-        except NotADirectoryError:
+        except NotADirectoryError:  # si el destino no es un directorio
             s_udp.sendto("El directorio no existe.".encode("utf-8"), addrCliente)
         except:
             s_udp.sendto("Algo salio mal".encode("utf-8"), addrCliente)
 
-    if opcion.decode("utf-8") == 'mv':
+    if opcion.decode("utf-8") == 'mv':  # el cliente elige mover un fichero
         print("El cliente ha selecionado mv")
         origen, addrCliente = s_udp.recvfrom(1024)
         destino, addrCliente = s_udp.recvfrom(1024)
