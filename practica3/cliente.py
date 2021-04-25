68a6a7315839d51b134a55aa87d03546445abe00
hanon11 <lucia.atienza2@gmail.com>
2021-04-25 12:24:10 +0200

    P3

--- practica3/cliente.py
+++ practica3/cliente.py
@@ -4,82 +4,92 @@ import json
 while True:
     while True:
         print("Elige qué opción desea realizar: ")
-        eleccion = int(input("\t 1. Dar de alta un nuevo miembro en el directorio.\n\t 2. Modificar los datos de un miembro.\n\t 3. Consultar la lista de todos los miembros de la Universidad." + 
-        "\n\t 4. Hacer consulta por DNI.\n\t 5. Consultar miembros según categoría.\n\t 6. Eliminar miembro ya existente.\n\t 7. Salir.\n"))
-        if (eleccion < 7 or eleccion > 0):  # si la eleccion se encuentra entre 1 y 6 (opciones validas)
+        eleccion = int(input(
+            "\t 1. Dar de alta un nuevo miembro en el directorio.\n\t 2. Modificar los datos de un miembro.\n\t 3. "
+            "Consultar la lista de todos los miembros de la Universidad." 
+            "\n\t 4. Hacer consulta por DNI.\n\t 5. Consultar miembros según categoría.\n\t 6. Eliminar miembro ya "
+            "existente.\n\t 7. Salir.\n"))
+        if eleccion < 7 or eleccion > 0:  # si la eleccion se encuentra entre 1 y 6 (opciones validas)
             break
-    if(eleccion == 1):
-        #dar de alta
+    if eleccion == 1:
+        # dar de alta
         print("Dar de alta un nuevo miembro.")
-        dni = input("    - DNI: ")    # leemos DNI del usuario
+        dni = input("    - DNI: ")  # leemos DNI del usuario
         nombre = input("    - Nombre completo: ")  # Leemos el nombre del usuario
         correo = input("    - Correo electrónico: ")  # Leemos correo
         departamento = input("    - Departamento: ")  # Leemos el departamento
         while True:
-            cat = input("    - Categoria (PAS/PDI/becario): ")    # Leemos la categoria del usuario
-            if(cat == "PAS" or cat == "PDI" or cat == "becario"):  # la categoria debe ser una de las 3
+            cat = input("    - Categoria (PAS/PDI/becario): ")  # Leemos la categoria del usuario
+            if cat == "PAS" or cat == "PDI" or cat == "becario":  # la categoria debe ser una de las 3
                 break
         asig = []
-        if(cat == "PDI"):  # si es PDI debe introducir las asignaturas
+        if cat == "PDI":  # si es PDI debe introducir las asignaturas
             while True:
                 asignatura = input("Introduce el nombre de la asignatura: (escribe exit para salir) ")
-                if(asignatura == 'exit'):
+                if asignatura == 'exit':
                     break
                 else:
                     asig.append(asignatura)
-        if (len(asig) == 0):  # si la lista no tiene asignaturas (usuario no PDI), pongo - (vacio)
+        if len(asig) == 0:  # si la lista no tiene asignaturas (usuario no PDI), pongo - (vacio)
             asig.append("-")
-        respuesta = requests.post('http://localhost:8080/AddMiembro', json={'dni' : dni, 'nombre' : nombre, 'correo' : correo, 'departamento' : departamento, 'cat' : cat, 'asig' : asig})
+        respuesta = requests.post('http://localhost:8080/AddMiembro',
+                                  json={'dni': dni, 'nombre': nombre, 'correo': correo, 'departamento': departamento,
+                                        'cat': cat, 'asig': asig})
         print(respuesta.text)  # Imprimimos 
 
-    if(eleccion == 2):
+    if eleccion == 2:
         dni = input("Introduce el DNI del usuario a modificar: ")
         print("Modificar los datos de un miembro.")
         nombre = input("    - Nombre completo: ")  # Leemos el nombre del usuario
         correo = input("    - Correo electrónico: ")  # Leemos correo
         departamento = input("    - Departamento: ")  # Leemos el departamento
         while True:
-            cat = input("    - Categoria (PAS/PDI/becario): ")    # Leemos la categoria del usuario
-            if(cat == "PAS" or cat == "PDI" or cat == "becario"):  # la categoria debe ser una de las 3
+            cat = input("    - Categoria (PAS/PDI/becario): ")  # Leemos la categoria del usuario
+            if cat == "PAS" or cat == "PDI" or cat == "becario":  # la categoria debe ser una de las 3
                 break
         asig = []
-        if(cat == "PDI"):  # si es PDI debe introducir las asignaturas
+        if cat == "PDI":  # si es PDI debe introducir las asignaturas
             while True:
                 asignatura = input("Introduce el nombre de la asignatura: (escribe exit para salir) ")
-                if(asignatura == 'exit'):
+                if asignatura == 'exit':
                     break
                 else:
                     asig.append(asignatura)
-        if (len(asig) == 0):  # si la lista no tiene asignaturas (usuario no PDI), pongo - (vacio)
+        if len(asig) == 0:  # si la lista no tiene asignaturas (usuario no PDI), pongo - (vacio)
             asig.append("-")
-    
-        respuesta = requests.put('http://localhost:8080/ModificarMiembro/' + str(dni), json={'nombre' : nombre, 'correo' : correo, 'departamento' : departamento, 'cat' : cat, 'asig' : asig})
+
+        respuesta = requests.put('http://localhost:8080/ModificarMiembro/' + str(dni),
+                                 json={'nombre': nombre, 'correo': correo, 'departamento': departamento, 'cat': cat,
+                                       'asig': asig})
         print(respuesta.text)  # Imprimimos 
 
-    if(eleccion == 3):
+    if eleccion == 3:
         print("Consultar la lista de todos los miembros de la Universidad.")
         respuesta = requests.get('http://localhost:8080/ListarMiembros')
         print(respuesta.text)  # Imprimimos 
 
-    if(eleccion == 4):
+    if eleccion == 4:
         print("Hacer consulta por DNI.")
         dni = input("Introduce el DNI del usuario: ")
         respuesta = requests.get('http://localhost:8080/BuscarMiembro/' + str(dni))
-        print(respuesta.text)  # Imprimimos 
+        print(respuesta.text)
 
-    if(eleccion == 5):
+
+    if eleccion == 5:
         print("Consultar miembros según categoría.")
         while True:
             cat = input("Introduce la categoría a consultar: ")  # Leemos la categoria del usuario
-            if(cat == "PAS" or cat == "PDI" or cat == "becario"):  # la categoria debe ser una de las 3
+            if cat == "PAS" or cat == "PDI" or cat == "becario":  # la categoria debe ser una de las 3
                 break
-        
-        respuesta = requests.get('http://localhost:8080/ConsultaCat/' + str(cat))
-        print(respuesta.text)  # Imprimimos 
 
-    if(eleccion == 6):
+        respuesta = requests.get('http://localhost:8080/ConsultaCat/' + str(cat))
+        print(respuesta.text)  # Imprimimos
+
+
+    if eleccion == 6:
         dni = input("Introduce el DNI del usuario: ")
         respuesta = requests.delete('http://localhost:8080/EliminarMiembro/' + str(dni))
-        print(respuesta.text)  # Imprimimos 
-    if(eleccion == 7):
+        print(respuesta.text)  # Imprimimos
+
+    if eleccion == 7:
         break
--- practica3/servidor.py
+++ practica3/servidor.py
@@ -1,8 +1,11 @@
 import json
 from bottle import run, request, response, get, post, put, delete
+import pickle
 # Simulamos base de datos en memoria
-miembros = dict() #key: dni, value: miembro
-# Creamos una clase Miembro con un constructor que tiene: dni, nombre, correo, departamento, categoria y lista de asignaturas que inicializamos
+miembros = dict()  # key: dni, value: miembro
+# Creamos una clase Miembro con un constructor que tiene: dni, nombre, correo, departamento, categoria y asignaturas
+
+
 class Miembro:
     def __init__(self, dni, nombre, correo, departamento, cat, asig):
         self.dni = dni
@@ -12,29 +15,32 @@ class Miembro:
         self.cat = cat
         self.asig = asig
 
+
 @post('/AddMiembro')
 def add_miembro():
     data = request.json
     # Datos del JSON
     dni = data.get("dni")
+    for id_miembros in miembros.keys():
+        if id_miembros == dni:
+            response.headers['Content-Type'] = 'application/json'  # Notificamos de que enviamos un archivo tipo JSON
+            return json.dumps("No ha sido posible dar de alta. Ya existe un miembro con ese DNI.")
     nombre = data.get("nombre")
     correo = data.get("correo")
     departamento = data.get("departamento")
     cat = data.get("cat")
     asig = data.get("asig")
     miembro = Miembro(dni, nombre, correo, departamento, cat, asig)
-    # persistencia (tendríamos que implementar los métodos de conexión con la base de datos etc.)
-    # save_data(course)
     # Añadimos instancia a nuestra base de datos
     miembros[dni] = miembro
-    # Una vez almacenada la instancia, es conveniente devolver una respuesta al usuario
-    # Construimos la cabecera: 'application/json' indicamos que vamos a devolver un JSON
+    # Indicamos que vamos a devolver un JSON
     response.headers['Content-Type'] = 'application/json'
     # Devolvemos el JSON
-    return json.dumps({'dni' : dni, 'nombre' : nombre, 'correo' : correo, 'departamento' : departamento, 'cat' : cat, 'asig' : asig})
+    return json.dumps({'dni': dni, 'nombre': nombre, 'correo': correo, 'departamento': departamento, 'cat': cat, 'asig': asig}, indent=2)
+
 
 @put('/ModificarMiembro/<dni_usuario>')
-def modificarMiembro(dni_usuario):
+def modificar_miembro(dni_usuario):
     datos = request.json
     try:
         miembros[dni_usuario].nombre = datos.get("nombre")
@@ -43,43 +49,62 @@ def modificarMiembro(dni_usuario):
         miembros[dni_usuario].cat = datos.get("cat")
         miembros[dni_usuario].asig = datos.get("asig")
         response.headers['Content-Type'] = 'application/json'
-        return json.dumps("Miembro modificado exitosamente.")  # Notificamos al cliente de que ha sido modificada la información del miembro
+        return json.dumps("Miembro modificado exitosamente.")  # Notificamos al cliente de que ha sido modificada la
+        # información del miembro
     except:  # si el miembro no existe
         response.headers['Content-Type'] = 'application/json'
-        return json.dumps("Miembro no encontrado.")  # Notificamos al cliente de que ha sido modificada la información del miembro
-
+        return json.dumps("Miembro no encontrado.")  # Notificamos al cliente de que ha sido modificada la
+        # información del miembro
 
 
 @get('/ListarMiembros')
-def listarmiembros():
+def listar_miembros():
     listamiembros = []    # listado de miembros que devolveremos
 
-    for dni_usuario, miembro in miembros.items(): # Recorremos el diccionario por objeto (clave y valor)
-        listamiembros.append({'dni' : miembro.dni, 'nombre' : miembro.nombre, 'correo' : miembro.correo, 'departamento' : miembro.departamento, 'cat' : miembro.cat, 'asig' : miembro.asig}) 
+    for dni_usuario, miembro in miembros.items():  # Recorremos el diccionario por objeto (clave y valor)
+        listamiembros.append({'dni': miembro.dni, 'nombre': miembro.nombre, 'correo': miembro.correo, 'departamento': miembro.departamento, 'cat': miembro.cat, 'asig': miembro.asig})
 
-    return json.dumps(listamiembros)  # Devolvemos la lista
+    return json.dumps(listamiembros, indent=2)  # Devolvemos la lista
 
 
 @get('/BuscarMiembro/<dni>')
-def buscarmiembro(dni):
-    return json.dumps({'dni' : miembros[dni].dni, 'nombre' : miembros[dni].nombre, 'correo' : miembros[dni].correo, 'departamento' : miembros[dni].departamento, 'cat' : miembros[dni].cat, 'asig' : miembros[dni].asig})  # Devolvemos la lista
+def buscar_miembro(dni):
+    return json.dumps({'dni': miembros[dni].dni, 'nombre': miembros[dni].nombre, 'correo': miembros[dni].correo, 'departamento': miembros[dni].departamento, 'cat': miembros[dni].cat, 'asig': miembros[dni].asig}, indent=2)
+    # Devolvemos la lista
+
 
 @get('/ConsultaCat/<cat>')
-def consultacat(cat):
+def consulta_cat(cat):
     listamiembros = []
-    for dni, miembro in miembros.items(): # Recorremos el diccionario 
-        if(miembro.cat == cat):
-            listamiembros.append({'dni' : miembro.dni, 'nombre' : miembro.nombre, 'correo' : miembro.correo, 'departamento' : miembro.departamento, 'cat' : miembro.cat, 'asig' : miembro.asig}) 
-    return json.dumps(listamiembros)  # Devolvemos la lista
+    for dni, miembro in miembros.items():  # Recorremos el diccionario
+        if miembro.cat == cat:
+            listamiembros.append({'dni': miembro.dni, 'nombre': miembro.nombre, 'correo': miembro.correo, 'departamento': miembro.departamento, 'cat': miembro.cat, 'asig': miembro.asig})
+    return json.dumps(listamiembros, indent=2)  # Devolvemos la lista
 
 
 @delete('/EliminarMiembro/<dni>')
-def eliminarmiembro(dni):
+def eliminar_miembro(dni):
     try:
         del miembros[dni]
         return json.dumps("Miembro borrado correctamente.")
     except:
         return json.dumps("Algo salió mal.")
 
+
+def guardar_datos(dic):
+    with open("miembros.dat", "wb") as f:
+        pickle.dump(dic, f)
+
+
+def carga_datos():
+    try:
+        with open("miembros.dat", "rb") as f:
+            return pickle.load(f)
+    except (OSError, IOError) as e:
+        return dict()
+
+
 if __name__ == '__main__':
+    miembros = carga_datos()
     run(host='localhost', port=8080, debug=True)
+    guardar_datos(miembros)
