7cca23ff8ec284e9e80ee9249c0bd69bef0e9a07
hanon11 <71835531+hanon11@users.noreply.github.com>
2021-05-27 13:05:06 +0200

    Delete 1

--- Intento de practica1/ejercicio4.py
+++
@@ -1,6 +0,0 @@
-#/usr/bin/ python3
-import subprocess
-with open('/etc/passwd', 'r') as fichero:
-    for linea in fichero:
-        valor = linea.split(':')
-        print( valor[0], 'tiene como directorio de inicio:', valor[5]) 
